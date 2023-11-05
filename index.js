7349e6e7b5e1a2ba2af4067357aa656fb886b5fb
MuhammadRukon <muhammad.rukon242@gmail.com>
2023-11-05 19:28:19 +0600

    api created for spefic data

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 require("dotenv").config();
+app.use(cors());
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.wgk6h9w.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -32,6 +34,16 @@ async function run() {
         console.log({ message: error });
       }
     });
+    // get specific data
+    app.get("/rooms/details/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await roomDB.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
